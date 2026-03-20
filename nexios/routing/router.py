c926a8d051b482a468c72e3068b2e2f51254eb5d
techwithdunamix@gmail.com <techwithdunamix@gmail.com>
2026-03-20 10:46:44 +0100

    refactor(routing): remove path params wrapping

--- nexios/routing/router.py
+++ nexios/routing/router.py
@@ -543,27 +543,7 @@ class Router(BaseRouter):
         route.tags = list(self.tags).extend(route.tags) if route.tags else self.tags
         if self.exclude_from_schema:
             route.exclude_from_schema = True
-        original_handler = route.handler
-
-        async def wrapped_handler(
-            request: Request, response: Response, **kwargs: Dict[str, Any]
-        ):
-            sig = inspect.signature(original_handler)
-            params = list(sig.parameters.keys())
-            handler_args = [request, response]
-            handler_kwargs = {}
-            if len(params) > 2:
-                # Get path parameters from request
-                path_params = request.path_params
-
-                # For parameters after the first two (request/response)
-                for param in params[2:]:
-                    if param in path_params:
-                        handler_kwargs[param] = path_params[param]
-
-            return await original_handler(*handler_args, **handler_kwargs)
-
-        route.handler = wrapped_handler
+        
         self.routes.append(route)
         if getattr(route, "exclude_from_schema", False):
             return
