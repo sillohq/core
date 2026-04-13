18149195548748ad1ff4dda7f7e604ec46d7ac9a
Chidebele Dunamis <dunamis.chidiebele@nkponani.com>
2026-04-13 13:29:43 +0100

    refactor(openapi): rename schema to spec to avoid issues with ty

--- nexios/testclient/_internal/types.py
+++ nexios/testclient/_internal/types.py
@@ -2,8 +2,7 @@ from __future__ import annotations
 
 import typing
 from collections.abc import Callable
-
-import anyio
+from anyio.abc import BlockingPortal
 
 from nexios.types import Receive, Scope, Send
 
@@ -12,4 +11,4 @@ ASGIInstance = Callable[[Receive, Send], typing.Awaitable[None]]
 ASGI2App = Callable[[Scope], ASGIInstance]
 ASGI3App = Callable[[Scope, Receive, Send], typing.Awaitable[None]]
 RequestData = typing.Mapping[str, str | typing.Iterable[str]]
-PortalFactoryType = typing.Callable[[], typing.ContextManager[anyio.abc.BlockingPortal]]
+PortalFactoryType = typing.Callable[[], typing.ContextManager[BlockingPortal]]
