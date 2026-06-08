db164e429b9ccf87b136eeddaf289ddb66f5e09c
Chidebele Dunamis <dunamis.chidiebele@nkponani.com>
2026-06-08 20:21:56 +0100

    fix(testclient): correct import path for WebSocketTestSession
    - Update import to use relative path `._internal.websockets` instead of `testclient._internal.websockets`
    - Remove unnecessary blank line after import statement
    - Fixes incorrect module reference in TYPE_CHECKING block

--- nexios/testclient/exceptions.py
+++ nexios/testclient/exceptions.py
@@ -3,8 +3,7 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
-    from testclient._internal.websockets import WebSocketTestSession
-
+    from ._internal.websockets import WebSocketTestSession
 
 class UpgradeException(Exception):
     def __init__(self, session: WebSocketTestSession) -> None:
